2867822d8eab0c5620a83e28f0118f02a3889700
Your Name <you@example.com>
2023-05-15 22:05:29 +0500

    7 task

--- script_hw2.js
+++ script_hw2.js
@@ -31,6 +31,16 @@
 
 //Задание 6
 
-let a = 9;
-let b = 2;
-alert(a % b);
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+//Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);

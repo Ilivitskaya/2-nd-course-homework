7ad0deff9832ac4e5ba66f6fce55681b73ca3812
Your Name <you@example.com>
2023-05-15 21:43:41 +0500

    3 task

--- script_hw2.js
+++ script_hw2.js
@@ -6,5 +6,10 @@
 
 //Задание 2
 
-let releaseYearIphone = 2007;
-alert(`год выпуска первого iPhone: ${releaseYearIphone}`);
+// let releaseYearIphone = 2007;
+// alert(`год выпуска первого iPhone: ${releaseYearIphone}`);
+
+//Задание 3
+
+let nameCreatorJs = "Брендан Эйх";
+alert(`Имя создателя языка JavaScript — ${nameCreatorJs}`);

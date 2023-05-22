598259ae79bb43e9c4b2706a350b461e938cb495
Your Name <you@example.com>
2023-05-22 21:58:33 +0500

    1 task

---
+++ hw3.js
@@ -0,0 +1,10 @@
+// Задание 1
+
+let password = '1234';
+let userEntered = prompt('Введи пароль');
+
+if (userEntered === password) {
+    console.log('Заходи!');
+} else {
+    console.log('Попробуй еще');
+}

3e5f31c7c0d29deea8bb5cfbd50c040b8629a9a0
Your Name <you@example.com>
2023-05-22 22:19:52 +0500

    3 task

--- hw3.js
+++ hw3.js
@@ -11,10 +11,22 @@
 
 // Задание 2
 
-let a = prompt('Введи число');
+// let a = prompt('Введи число');
 
-if (a > 0 && a < 10) {
+// if (a > 0 && a < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// Задание 3
+
+let d = 2;
+let e = 7;
+
+if (d > 100 || e > 100) {
     console.log('Верно');
 } else {
     console.log('Неверно');
 }
+

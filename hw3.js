bb6620c6799dc42656f8c6cf4f49aeae4086bab1
Your Name <you@example.com>
2023-05-22 22:14:40 +0500

    2 task

--- hw3.js
+++ hw3.js
@@ -1,10 +1,20 @@
 // Задание 1
 
-let password = '1234';
-let userEntered = prompt('Введи пароль');
+// let password = '1234';
+// let userEntered = prompt('Введи пароль');
 
-if (userEntered === password) {
-    console.log('Заходи!');
+// if (userEntered === password) {
+//     console.log('Заходи!');
+// } else {
+//     console.log('Попробуй еще');
+// }
+
+// Задание 2
+
+let a = prompt('Введи число');
+
+if (a > 0 && a < 10) {
+    console.log('Верно');
 } else {
-    console.log('Попробуй еще');
+    console.log('Неверно');
 }

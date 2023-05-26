a431bc93cccfc45298d6e808beb9100a2149be64
Your Name <you@example.com>
2023-05-26 22:52:01 +0500

    4 task

--- hw4.js
+++ hw4.js
@@ -17,9 +17,21 @@
 
 // Задание 3
 
-let num = 7;
+// let num = 7;
 
-while (num < 23) {
-    console.log(num);
-    num++;
+// while (num < 23) {
+//     console.log(num);
+//     num++;
+// }
+
+// Задание 4
+
+const obj = {
+	Коля: 200,
+	Вася: 300,
+	Петя: 400,
+}
+
+for (const key in obj) {
+    alert(`${key} - зарплата ${obj[key]} долларов.`)
 }

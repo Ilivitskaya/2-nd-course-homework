0a22da13c642299d74df174b2e45a584077d24ba
Your Name <you@example.com>
2023-05-26 22:21:31 +0500

    1 task

--- hw4.js
+++ hw4.js
@@ -1,7 +1,16 @@
 // Задание 1
 
-let a = 1
-while (a <= 2) {
-    console.log('Привет');
-    a++;
+// let a = 1
+// while (a <= 2) {
+//     console.log('Привет');
+//     a++;
+// }
+
+// Задание 2
+
+let b = 1
+
+while (b < 6) {
+    console.log(b);
+    b++;
 }

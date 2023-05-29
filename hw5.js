b21dface1e13dff6adbae2f287fe13344ddd8046
Your Name <you@example.com>
2023-05-29 22:25:06 +0500

    2 task

--- hw5.js
+++ hw5.js
@@ -1,7 +1,16 @@
 // 1 task
 
-function lessNumber(a, b) {
-    return a < b ? console.log(a) : console.log(b);
+// function lessNumber(a, b) {
+//     return a < b ? console.log(a) : console.log(b);
+// };
+
+// lessNumber(110, 100);
+
+// 2 task
+
+let evenNum = (num) => {
+    return num % 2 == 0 ? console.log(`Число четное`) : console.log(`Число нечетное`);
 };
 
-lessNumber(110, 100);
+let result = evenNum(44);
+

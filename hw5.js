b2414133cb93cd9c5cc8b7a2db93e7e4aa0ba169
Your Name <you@example.com>
2023-05-31 12:43:17 +0500

    3 task

--- hw5.js
+++ hw5.js
@@ -8,9 +8,26 @@
 
 // 2 task
 
-let evenNum = (num) => {
-    return num % 2 == 0 ? console.log(`Число четное`) : console.log(`Число нечетное`);
+// let evenNum = (num) => {
+//     return num % 2 == 0 ? console.log(`Число четное`) : console.log(`Число нечетное`);
+// };
+
+// let result = evenNum(44);
+
+// 3 task
+
+function adultUser(age) {
+    let userAge = prompt(`сколько вам лет?`);
+
+    if (userAge >= 13) {
+        alert(`Добро пожаловать!`)
+    } else if (userAge > 0 && userAge < 13) {
+        alert(`Привет, друг!`);
+    } else {
+        alert(`Вы ввели неправильное значение`)
+    }
 };
 
-let result = evenNum(44);
+adultUser();
+
 

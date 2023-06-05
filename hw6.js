c9528f267c1fda5408541519328b2a499396a9ee
Your Name <you@example.com>
2023-06-05 21:41:59 +0500

    4 task

--- hw6.js
+++ hw6.js
@@ -13,9 +13,21 @@
 
 
 // 3 task
-let numbers = [1, 3, 5, 10, 20];
+// let numbers = [1, 3, 5, 10, 20];
 
-numbers = numbers.join(' ');
-console.log(numbers);
+// numbers = numbers.join(' ');
+// console.log(numbers);
 
+// 4 task
+// С помощью вложенных циклов создайте многомерный массив вида: [[1, 1, 1], [1, 1, 1], [1, 1, 1]].
 
+let arr = [];
+        for (let i = 0; i < 3; i++)
+        {
+            arr[i] = [];
+            for (let j = 0; j < 3; j++)
+            {
+                arr[i][j] = 1;
+            }
+        }
+        console.log(arr);

c904bdb76acbf7b9d23cd382e587265728112aaf
Your Name <you@example.com>
2023-06-29 22:11:45 +0500

    save all

---
+++ script.js
@@ -0,0 +1,16 @@
+function checkSeason() {
+    
+    let monthNumber = prompt('Напиши номер месяца');
+
+    if (monthNumber > 2 && monthNumber < 6) {
+        alert(`весна`);
+    } else if (monthNumber > 5 && monthNumber < 9) {
+        alert(`лето`);
+    } else if (monthNumber > 8 && monthNumber < 12) {
+        alert(`осень`);
+    } else if (monthNumber > 0 && monthNumber < 3 || monthNumber == 12) {
+        alert(`зима`);
+    } else {
+        alert(`нет такого месяца`);
+    }
+}

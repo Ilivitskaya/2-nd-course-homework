9567ce0355fde8ba895e1085fa8b38f3da75a7e0
Your Name <you@example.com>
2023-07-12 11:36:08 +0500

    add changes

--- hw8.js
+++ hw8.js
@@ -10,46 +10,39 @@
 //  console.log(people.sort((a, b) => a.age - b.age));
 
 
-//task 2
+// task 2
 
-// function isPositive(num) {
-// 	if (num > 0) {
-// 		return num;
-// 	};
-	
-// }
+function filter (arr, ruleFunction) {
 
-// function filter(arr, ruleFunction) {
-// 	let tempr = 0;
-// 	const output = [];
-// 	for (let i = 0; i < arr.length; i++) {
-// 		tempr = ruleFunction(arr[i]);
-// 		if (tempr != undefined) {
-// 			output.push(tempr);
-// 		}
-// 	}
-	
-// 	return output;
-// 	;
-// }
+	const output = [];
 
-// console.log(filter([3, -4, 1, 9], isPositive));
-// // Должен выводить [3, 1, 9]
+	for (let i = 0; i < arr.length; i++) {
+		if (ruleFunction(arr[i]) === true) {
+			output.push(arr[i]);
+		}
+	}
+  
+	return output;
+  }
+
+function isPositive(num) {
+	return num > 0;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
 
 
-// function isMale(obg) {
-// 	if (obg.gender === `male`) {
-// 		return obg;
-// 	}
-// }
-// const people = [
-// 	{ name: 'Глеб', gender: 'male' },
-// 	{ name: 'Анна', gender: 'female' },
-// 	{ name: 'Олег', gender: 'male' },
-// 	{ name: 'Оксана', gender: 'female' }
-// ];
+function isMale(obg) {
+	return obg.gender === `male`;
+}
+const people = [
+	{ name: 'Глеб', gender: 'male' },
+	{ name: 'Анна', gender: 'female' },
+	{ name: 'Олег', gender: 'male' },
+	{ name: 'Оксана', gender: 'female' }
+];
 
-// console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
+console.log(filter(people, isMale));
 
 // task 3
 
@@ -78,17 +71,20 @@
 
 // task 5
 
-function delayForSecond(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-				if(cb) { 	cb(); }
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+// 				if(cb) { 	cb(); }
 
-    }, 1000)
-}
+//     }, 1000)
+// }
 
-function sayHi (name) {
-    console.log(`Привет, ${name}!`);
+// function sayHi (name) {
+//     console.log(`Привет, ${name}!`);
 
-}
+// }
 
-sayHi(`Глеб`, delayForSecond());
+// delayForSecond(() => sayHi('Глеб'));
+
+
+
